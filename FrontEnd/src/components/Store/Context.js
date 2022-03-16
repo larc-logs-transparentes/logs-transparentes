e43467e2372d74453e8a9d0e010a5b013321dc9e
jonrato <jonatas_ferreira@usp.br>
2022-03-16 17:49:18 -0300

    clear code

--- FrontEnd/src/components/Store/Context.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-
-const StoreContext = createContext({
-    token: null,
-    setToken: () => {};
-})
-
-export default StoreContext;
--- FrontEnd/src/pages/Login/Login.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import UserLogin from 'components/User/Login/Login';
-
-const PagesLogin = () => (
-  <UserLogin />
-);
-
-export default PagesLogin;
