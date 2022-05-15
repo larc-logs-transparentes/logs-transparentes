e0041d7c0c35474a8326116c451069ded13ad4de
Guilherme <guilhermefumagali@estudante.ufscar.br>
2022-05-15 10:48:45 -0300

    Integrando pub

--- bu_backend/src/controllers/bu.controller.js
+++ bu_backend/src/controllers/bu.controller.js
@@ -1,6 +1,19 @@
 const db = require("../models");
 const merkletree_adapter = require("../adapters/merkletree.adapter")
 
+/* ----------------------------------- */
+const mqtt = require('mqtt');
+
+const consistencyProofData = {
+  raizAssinada: null,
+  BUsAdicionados: [],
+  cont: 0,
+  ultimo: false,
+}
+
+const TAM_MTREE_PARCIAL = 4
+/* ----------------------------------- */
+
 const BU = db.bu;
 
 // Create and Save a new BU
@@ -9,13 +22,30 @@ exports.create = (data) => {
   console.log("Debug BU")
   console.log(buString)
   console.log({"BU": data})
-  return merkletree_adapter.addLeaf(buString).then((merkletree_data) => {
+
+  merkletree_adapter.addLeaf(buString).then((merkletree_data) => {
     BU.create({
       merkletree_leaf_id: merkletree_data.leaf_index,
       merkletree_leaf: merkletree_data.added_leaf,
       ...data
     })
+
+    consistencyProofData.BUsAdicionados.push(merkletree_data.added_leaf)
+    console.log(merkletree_data.added_leaf + " " + consistencyProofData.BUsAdicionados.length +" adicionado ao buffer de BUs")
   })
+
+  if(consistencyProofData.BUsAdicionados.length >= TAM_MTREE_PARCIAL){
+    merkletree_adapter.getTreeRoot().then((treeRoot) => {
+      consistencyProofData.raizAssinada = treeRoot
+    })
+
+    publish("guilherme/teste", JSON.stringify(consistencyProofData))
+    console.log("Publicado teste de consistência")
+    consistencyProofData.BUsAdicionados = []
+    consistencyProofData.cont ++
+  }
+
+  return
 };
 
 
@@ -48,3 +78,12 @@ exports.findByInfo = (turno, uf, zona, secao) => {
     return data
   })
 };
+
+function publish(topic, payload){
+  const client  = mqtt.connect('mqtt://test.mosquitto.org')
+
+  client.on('connect', function () {
+      client.publish(topic, payload, {qos: 2})
+      client.end()
+  })
+}
