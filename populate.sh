404ed2a13a32a22d27627b68fa61f85e5500749b
Gustav <59322464+GustavsC@users.noreply.github.com>
2022-06-14 09:32:42 -0300

    Add files via upload

--- populate.sh
+++ populate.sh
@@ -1,97 +1,31 @@
 DATAS=(
     '{
+        "_id":"0",
+        "id": "0",
         "secao": "001", 
         "zona": "123", 
         "UF": "SP", 
         "turno": "1",
+        "__v": "0",
         "votos":[
-            {"nome": "Candidado A", "votos": 98, "partido": "XX"},
-            {"nome": "Candidado B", "votos": 36, "partido": "YY"}
+            {"partido": "XX", "nome": "Candidado A", "votos": 198, "_id": "2"},
+            {"partido": "YY", "nome": "Candidado B", "votos": 36,  "_id": "1"}
     ]}'
     '{
+        "_id":"1",
+        "id": "1",
         "secao": "002", 
         "zona": "123", 
         "UF": "SP", 
         "turno": "1",
+        "__v": "0",
         "votos":[
-            {"nome": "Candidado A", "votos": 41, "partido": "XX"},
-            {"nome": "Candidado B", "votos": 9, "partido": "YY"}
-    ]}'
-    '{
-        "secao": "003", 
-        "zona": "123", 
-        "UF": "SP", 
-        "turno": "1",
-        "votos":[
-            {"nome": "Candidado A", "votos": 10, "partido": "XX"},
-            {"nome": "Candidado B", "votos": 2, "partido": "YY"}
-    ]}'
-    '{
-        "secao": "004", 
-        "zona": "123", 
-        "UF": "SP", 
-        "turno": "1",
-        "votos":[
-            {"nome": "Candidado A", "votos": 51, "partido": "XX"},
-            {"nome": "Candidado B", "votos": 43, "partido": "YY"}
-    ]}'
-    '{
-        "secao": "005", 
-        "zona": "123", 
-        "UF": "SP", 
-        "turno": "1",
-        "votos":[
-            {"nome": "Candidado A", "votos": 38, "partido": "XX"},
-            {"nome": "Candidado B", "votos": 76, "partido": "YY"}
-    ]}'
-    '{
-        "secao": "006", 
-        "zona": "123", 
-        "UF": "SP", 
-        "turno": "1",
-        "votos":[
-            {"nome": "Candidado A", "votos": 40, "partido": "XX"},
-            {"nome": "Candidado B", "votos": 83, "partido": "YY"}
-    ]}'
-    '{
-        "secao": "007", 
-        "zona": "123", 
-        "UF": "SP", 
-        "turno": "1",
-        "votos":[
-            {"nome": "Candidado A", "votos": 30, "partido": "XX"},
-            {"nome": "Candidado B", "votos": 29, "partido": "YY"}
-    ]}'
-    '{
-        "secao": "008", 
-        "zona": "123", 
-        "UF": "SP", 
-        "turno": "1",
-        "votos":[
-            {"nome": "Candidado A", "votos": 38, "partido": "XX"},
-            {"nome": "Candidado B", "votos": 83, "partido": "YY"}
-    ]}'
-    '{
-        "secao": "009", 
-        "zona": "123", 
-        "UF": "SP", 
-        "turno": "1",
-        "votos":[
-            {"nome": "Candidado A", "votos": 98, "partido": "XX"},
-            {"nome": "Candidado B", "votos": 65, "partido": "YY"}
-    ]}'
-    '{
-        "secao": "010", 
-        "zona": "123", 
-        "UF": "SP", 
-        "turno": "1",
-        "votos":[
-            {"nome": "Candidado A", "votos": 8, "partido": "XX"},
-            {"nome": "Candidado B", "votos": 37, "partido": "YY"}
+            {"partido": "XX", "nome": "Candidado A", "votos": 41,"_id": "5"},
+            {"partido": "YY", "nome": "Candidado B", "votos": 109,"_id": "6"}
     ]}'
 )
 
-for i in {0..9}
+for i in {0..1}
 do
     curl -X POST -H "Content-Type: application/json" --data "${DATAS[i]}" localhost:8080/bu
 done
