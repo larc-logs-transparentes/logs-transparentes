9232c720d9960fcc889876a31da61b9c39e47a57
= <=>
2022-08-17 16:57:11 -0300

    Gerando prova de consistência no Pub

--- mqtt/mqttPub.js
+++ mqtt/mqttPub.js
@@ -1,7 +1,7 @@
 const { MerkleTree } = require('merkletreejs')
 const SHA256 = require('crypto-js/sha256')
-
 const mqtt = require('mqtt')
+
 
 let consistencyProofData = {
     tree_size_1: null,
@@ -15,81 +15,14 @@ let consistencyProofData = {
 
 /* ------------------------- 1º envio ------------------------------ */
 
-BUs = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-const TSEMerkleTree = new MerkleTree(BUs.map(x => SHA256(x)), SHA256)
-
-consistencyProofData.first_hash = null;
-consistencyProofData.tree_size_1 = 0;
-consistencyProofData.second_hash = TSEMerkleTree.getRoot().toString('hex')
-consistencyProofData.tree_size_2 = TSEMerkleTree.getLeafCount()
-consistencyProofData.consistency_path = BUs.map(x => SHA256(x).toString())
-consistencyProofData.log_id = 0
-
-publish('guilherme/teste', JSON.stringify(consistencyProofData))
-
-/* ------------------------- 2º envio ------------------------------ */
-  
-consistencyProofData.first_hash = consistencyProofData.second_hash;
-consistencyProofData.tree_size_1 = consistencyProofData.first_hash;
-
-BUs = ['i', 'j', 'k', 'l']
-TSEMerkleTree.addLeaves(BUs.map(x => SHA256(x)))
- 
-consistencyProofData.second_hash = TSEMerkleTree.getRoot().toString('hex')
-consistencyProofData.tree_size_2 = TSEMerkleTree.getLeafCount()
-consistencyProofData.consistency_path = BUs.map(x => SHA256(x).toString())
-consistencyProofData.log_id = 1
-
-publish('guilherme/teste', JSON.stringify(consistencyProofData))
-
-/* ------------------------- 3º envio ------------------------------ */
- 
-BUs = ['m', 'n']
-TSEMerkleTree.addLeaves(BUs.map(x => SHA256(x)))
- 
-consistencyProofData.raizAssinada = TSEMerkleTree.getRoot().toString('hex')
-consistencyProofData.BUsAdicionados = BUs.map(x => SHA256(x).toString())
-consistencyProofData.cont = 2
-
-//publish('guilherme/teste', JSON.stringify(consistencyProofData))
-
-/* ------------------------- 4º envio ------------------------------ */
- 
-BUs = ['o', 'p']
-TSEMerkleTree.addLeaves(BUs.map(x => SHA256(x)))
- 
-consistencyProofData.raizAssinada = TSEMerkleTree.getRoot().toString('hex')
-consistencyProofData.BUsAdicionados = BUs.map(x => SHA256(x).toString())
-consistencyProofData.cont = 3
-
-//publish('guilherme/teste', JSON.stringify(consistencyProofData))
- 
-/* ------------------------- 5º envio ------------------------------ */
-
-BUs = ['q', 'r']
-TSEMerkleTree.addLeaves(BUs.map(x => SHA256(x)))
- 
-consistencyProofData.raizAssinada = TSEMerkleTree.getRoot().toString('hex')
-consistencyProofData.BUsAdicionados = BUs.map(x => SHA256(x).toString())
-consistencyProofData.cont = 4
-
-//publish('guilherme/teste', JSON.stringify(consistencyProofData))
- 
-/* ------------------------- 6º envio ------------------------------ */
-
-BUs = ['s', 't', 'v']
-TSEMerkleTree.addLeaves(BUs.map(x => SHA256(x)))
-
-consistencyProofData.raizAssinada = TSEMerkleTree.getRoot().toString('hex')
-consistencyProofData.BUsAdicionados = BUs.map(x => SHA256(x).toString())
-consistencyProofData.cont = 5
-consistencyProofData.ultimo = true
-
-//publish('guilherme/teste', JSON.stringify(consistencyProofData))
-/* -------------------------------------------------------------- */
-
-console.log(TSEMerkleTree.toString())
-console.log("Dados publicados")
+leaves = ['d0', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6']
+for (const key in leaves) 
+    console.log(`${leaves[key]} ${SHA256(leaves[key])}`)
+console.log("------")
+const MT = new MerkleTree(leaves.map(x => SHA256(x)), SHA256)
+aux = MT.getLeaves()
+MT.print()
+console.log(proof(6, aux))
 
 function publish(topic, payload){
     const client  = mqtt.connect('ws://localhost:3031')
@@ -98,4 +31,59 @@ function publish(topic, payload){
         client.publish(topic, payload, {qos: 2})
         client.end()
     })
+}
+
+/**
+* proof
+* @desc 
+* @param {Number} m - previous Merkle Tree number of leaves
+* @param {Buffer[]} D_n - ordered list of n inputs to the tree
+* @return {Buffer[]}
+* @example
+*```js
+*const leaves = tree.getLeaves()
+*```
+*/
+function proof(m, D){
+    n = D.length
+    if(0 < m && m < n)
+        return subProof(m, D, true)
+    return null
+}
+
+function subProof(m, D, subTree){
+    const path = []
+    const n = D.length
+    
+    if (m == n) {
+        if (!subTree)
+            path.push(MTH(D))
+        return path
+    }
+
+    if(m < n){
+        k = maiorPot2MenorQue(n)
+        if (m <= k){
+            //a subárvore da direita D[k:n] só existe na árvore atual
+            //então, temos que provar que a subárvore da esquerda D[0:k]  
+            path.push(subProof(m, D.slice(0, k), subTree))
+            //e adicionar ao pacote a ser enviado D[k:n]
+            path.push(MTH(D.slice(k,n)))
+        } else {
+            //a subárvore da esquerda são idênticas em ambas as árvores 
+            //Vamos provar que a subárvore da direita D[k:n] são consistentes
+            path.push(subProof(m - k, D.slice(k, n), false))
+            //e adicionar ao pacote a ser enviado D[0:k]
+            path.push(MTH(D.slice(0,k)))
+        }
+    }
+    return path
+}
+
+function MTH(D){
+    return new MerkleTree(D, SHA256).getRoot()
+}
+
+function maiorPot2MenorQue(n){
+    return Math.pow(2,parseInt(Math.log2(n)));
 }
