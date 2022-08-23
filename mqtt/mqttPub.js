41ee2cb731b7909c0447141266a78a3b20115be6
= <=>
2022-08-23 11:57:08 -0300

    proof funcionando com os exemplos

--- mqtt/mqttPub.js
+++ mqtt/mqttPub.js
@@ -2,7 +2,6 @@ const { MerkleTree } = require('merkletreejs')
 const SHA256 = require('crypto-js/sha256')
 const mqtt = require('mqtt')
 
-
 let consistencyProofData = {
     tree_size: null,
     tree_hash: null,
@@ -14,28 +13,27 @@ let consistencyProofData = {
 /* ------------------------- 1º envio ------------------------------ */
 
 leaves = ['d0', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6']
-for (const key in leaves) 
+/* for (const key in leaves) 
     console.log(`${leaves[key]} ${SHA256(leaves[key])}`)
-console.log("------")
+console.log("------") */
 const MT = new MerkleTree(leaves.map(x => SHA256(x)), SHA256)
-aux = MT.getLeaves()
+leaves = MT.getLeaves()
 MT.print()
 console.log("-- m = 3 --")
-console.log(proof(3, aux))
-consistencyProofData.tree_size = MT.getLeafCount()
-consistencyProofData.tree_hash = MT.getRoot()
-consistencyProofData.consistency_path = proof(3, aux)
-
+console.log(proof(3, leaves))
 console.log("-- m = 4 --")
-console.log(proof(4, aux))
+console.log(proof(4, leaves))
 console.log("-- m = 6 --")
-console.log(proof(6, aux))
+console.log(proof(6, leaves))
 
-
-
+/**
+* publish
+* @desc - publica os dados contidos no payload no tópico correspondente
+* @param {String} topic
+* @param {any} payload - lista ordenada das n entradas da árvore
+*/
 function publish(topic, payload){
     const client  = mqtt.connect('ws://localhost:3031')
-
     client.on('connect', function () {
         client.publish(topic, payload, {qos: 2})
         client.end()
@@ -44,20 +42,34 @@ function publish(topic, payload){
 
 /**
 * proof
-* @desc 
-* @param {Number} m - previous Merkle Tree number of leaves
-* @param {Buffer[]} D_n - ordered list of n inputs to the tree
+* @desc - retorna a lista de nós da Merkle Tree necessários para verificar que as m primeiras entradas D[0:m] são iguais em ambas as árvores.
+* @param {Number} m - número de folhas da Merkle Tree anterior
+* @param {Buffer[]} D_n - lista ordenada das n entradas da árvore
 * @return {Buffer[]}
-* @example
-*```js
-*const leaves = tree.getLeaves()
-*```
 */
 function proof(m, D){
     n = D.length
     if(0 < m && m < n)
-        return __subProof(m, D, true)
+        
+    return __subProof(m, D, true)
     return null
+}
+
+/**
+* MTH
+* @desc - calcula a raiz da Merkle Tree com as entradas D
+* @param {Buffer[]} D - lista ordenada de entradas
+* @return {Buffer}
+*/
+function MTH(D){
+    n = D.length
+
+    if (n == 0)
+        return SHA256(null).toString()
+    if (n == 1)
+        return D[0]
+
+    return new MerkleTree(D, SHA256).getRoot()
 }
 
 function __subProof(m, D, subTree){
@@ -69,55 +81,24 @@ function __subProof(m, D, subTree){
         return path
     }
     if(m < n){
-        k = maiorPot2MenorQue(n)
+        const k = __maiorPot2MenorQue(n)
         if (m <= k){
             //a subárvore da direita D[k:n] só existe na árvore atual
-            //então, temos que provar que a subárvore da esquerda D[0:k]  
+            //então, temos que provar que a subárvore da esquerda D[0:k] é consistente
             path.push(__subProof(m, D.slice(0, k), subTree))
-            //e adicionar ao pacote a ser enviado D[k:n]
+            //e adicionar ao pacote a ser enviado D[k:n] a subárvore da direita
             path.push(MTH(D.slice(k,n)))
         } else {
             //a subárvore da esquerda são idênticas em ambas as árvores 
-            //Vamos provar que a subárvore da direita D[k:n] são consistentes
-            path.push(__subProof(m - k, D.slice(k, n), false))
-            //e adicionar ao pacote a ser enviado D[0:k]
+            //Vamos provar que a subárvore da direita D[k:n] é consistente
+            path.push(__subProof(m - k, D.slice(k,n), false))
+            //e adicionar ao pacote a ser enviado D[0:k] a subárvore da esquerda
             path.push(MTH(D.slice(0,k)))
         }
     }
     return path.flat()
 }
 
-function MTH(D){
-    n = D.length
-    if (n == 0)
-        return SHA256(null).toString()
-    if (n == 1){
-        var h = [0x00, D[0]]
-        return SHA256(Buffer.concat(h)).toString()
-    }
-
-    k = maiorPot2MenorQue(n)
-
-    var c = []
-    c[0] =  0x01
-    x = MTH(D.slice(0, k))
-    c.concat(x)
-    x = MTH(D.slice(k, n))
-    c.concat(x)
-    return SHA256(c).toString()
-}
-
-function maiorPot2MenorQue(n){
-    /* if (n < 2)
-        return 0
-    t = 0
-    for (let i = 0; i < 64; i++) {
-        c = 1 << i;
-        if (c > n-1)
-            return t
-        t = c
-    }
-    return 0 
-     */
+function __maiorPot2MenorQue(n){
     return Math.pow(2,parseInt(Math.log2(n-1)));
 }
