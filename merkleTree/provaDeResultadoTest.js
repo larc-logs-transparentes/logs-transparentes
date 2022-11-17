f50b6bc7affe96016657a1d86380426edd90f86b
gfumagali <guilhermefumarques@gmail.com>
2022-11-16 22:40:01 -0300

    Coordenadas

--- merkleTree/provaDeResultadoTest.js
+++ merkleTree/provaDeResultadoTest.js
@@ -1,5 +1,32 @@
-const { SHA256 } = require("crypto-js");
 const { MerkleTree }  = require("merkletreejs");
+const SHA256 = require("crypto-js/sha256");
+
+function nodeKeys(leaves, i_inicial, i_final) {
+    return __nodeKeys(leaves, i_inicial, i_final, 0, 0)
+}
+
+function __nodeKeys(leaves, i_inicial, i_final, depth, index){
+    const nodes = []
+    const n = leaves.length
+    if (n == 1 || (i_inicial == 0 && i_final == n - 1)) 
+        return{depth: depth, index: index, hash: MTH(leaves)}
+
+    const p = largerPowerOfTwoLessThan(n)
+    if(i_inicial < p)
+        nodes.push(__nodeKeys(leaves.slice(0, p), i_inicial, Math.min(i_final, p - 1), depth + 1, index * 2))
+    if(i_final >= p)
+        nodes.push(__nodeKeys(leaves.slice(p, n), Math.max(i_inicial, p) - p, i_final - p, depth + 1, index * 2 + 1))
+    
+    return nodes.flat()
+}
+
+function largerPowerOfTwoLessThan(n) {
+    return Math.pow(2,parseInt(Math.log2(n-1)));
+}
+
+function MTH(leaves){
+    return new MerkleTree(leaves.map((leaf) => SHA256(leaf)), SHA256, {fillDefaultHashes: true}).getRoot().toString('hex')
+}
 
 const leaves = ["c0", "c1", "c2", "c3", "c4", "c5", "c6", "c7"];
 const merkleTree = new MerkleTree(leaves.map((leaf) => SHA256(leaf)), SHA256, {fillDefaultHashes: true});
@@ -13,31 +40,3 @@ console.log()
 console.log(nodeKeys(leaves, 4, 7))
 console.log()
 console.log(nodeKeys(leaves, 0, 1))
-
-
-function nodeKeys(leaves, i_inicial, i_final) {
-    return __nodeKeys(leaves, i_inicial, i_final)
-}
-
-function __nodeKeys(leaves, i_inicial, i_final){
-    const nodes = []
-    const n = leaves.length
-    if (n == 1 || (i_inicial == 0 && i_final == n - 1)) 
-        return MTH(leaves)
-        
-    const p = largerPowerOfTwoLessThan(n)
-    if(i_inicial < p)
-        nodes.push(__nodeKeys(leaves.slice(0, p), i_inicial, Math.min(i_final, p - 1)))
-    if(i_final >= p)
-        nodes.push(__nodeKeys(leaves.slice(p, n), Math.max(i_inicial, p) - p, i_final - p))
-    
-    return nodes.flat()
-}
-
-function largerPowerOfTwoLessThan(n) {
-    return Math.pow(2,parseInt(Math.log2(n-1)));
-}
-
-function MTH(leaves){
-    return new MerkleTree(leaves.map((leaf) => SHA256(leaf)), SHA256, {fillDefaultHashes: true}).getRoot().toString('hex')
-}
