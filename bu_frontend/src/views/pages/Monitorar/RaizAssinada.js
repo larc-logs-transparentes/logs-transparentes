06f1f0ed5dcdb7e504b21d6d20e523b783895aab
Ga4breu <gabriel_schaedler@hotmail.com>
2022-12-13 14:54:51 -0300

    Ajustando o front para os objetos

--- bu_frontend/src/views/pages/Monitorar/RaizAssinada.js
+++ bu_frontend/src/views/pages/Monitorar/RaizAssinada.js
@@ -59,41 +59,42 @@ export default function Raizassinada() {
               <div style={{display:'flex-column',margin:'auto', textAlign:'center',position:'static',marginTop:'30vh'}}>
               <div style={{display:'flex',position:'relative',gap:'0.5vw',textAlign:'center',fontSize:'30px',padding:'.3vw',marginTop:'-20vh',marginLeft:'auto',marginRight:'auto'}}>
                 <div >
-                    <h4>ID</h4>
+                    <h5>ID</h5>
                     {rootsbaixadas.map(rootsbaixadas => <h5 style={{color:'black',backgroundColor:'#c4c4c4',padding:'.4vw',borderRadius:'2px'}}>{rootsbaixadas._id}</h5>)}
                 </div>
                 <div>
-                    <h4>Raiz Parcial</h4>
+                    <h5>Raiz Parcial</h5>
                     {rootsbaixadas.map(rootsbaixadas=> <h5 style={{color:'black',backgroundColor:'#c4c4c4',padding:'.4vw',borderRadius:'2px',textAlign:'justify'}}>
                      {rootsbaixadas.second_hash}
                      </h5>)}
                 </div>
                 <div>
-                  <h4 style={{}}>Tamanho da Árvore</h4>
+                  <h5 style={{width:'170px'}}>Tamanho da Árvore</h5>
                   {rootsbaixadas.map(rootsbaixadas => 
                     <h5 style={{color:'black',backgroundColor:'#c4c4c4',padding:'.4vw',position:'flex'}}>
                       {rootsbaixadas.tree_size_2}
                     </h5>)}
                 </div> 
                 <div>
-                <h4>Validação</h4>
+                <h5>Validação</h5>
                   {vetorvalidar.map(vetorvalidar=> 
                     <h5 style={{color:'black',backgroundColor:'#c4c4c4',padding:'.4vw'}}>
                       {vetorvalidar}
                     </h5>)}
                 </div>
                 <div>
-                <h4>Assinatura</h4>
+                <h5>Assinatura</h5>
                   {vetorvalidar.map(vetorvalidar=> 
                     <h5 style={{color:'black',backgroundColor:'#c4c4c4',padding:'.4vw'}}>
-                      {'3c3b72a69d0e25c9b2abc0d636953bfa932891f7'}
+                      {'3c3b72a69d0e25c9b2abc0d636'}
                     </h5>)}
                 </div>
               </div>
-              <div style={{display:'flex',gap:'4vw', marginTop:'10vw',textAlign:'start'}}>
+              <div style={{display:'inline-block',columnCount:'3',gap:'6vw', marginTop:'10vw',marginRight:'20vw',textAlign:'start',alignContent:'start'}}>
               {rootbaixada.map( ( {_id,tree_size_1,tree_size_2,first_hash,second_hash,consistency_path,log_id,__v} ) => {
-                return <p style={{width:'10vw',gap:'4vw'}} key={_id}>tree_size_1 :{tree_size_1}<br/>tree_size_2 :{tree_size_2}<br/>first_hash :{first_hash}<br/>
-                second_hash:{second_hash}<br/>consistency_path:{consistency_path}<br/>log_id:{log_id}<br/></p>})}
+                return <p style={{width:'15vw'}} key={_id}>log_id:{log_id}<br/> tree_size_1 :{tree_size_1}<br/>tree_size_2 :{tree_size_2}<br/>first_hash :{first_hash}<br/>
+                second_hash:{second_hash}<br/>consistency_path:{consistency_path.map(consistency_path => <li>{consistency_path}</li>)}
+                <br/> ----------------------------------------------------<br/></p>})}
               </div>
             </div>
             </div>
