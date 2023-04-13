a7f4c78bd26482e1716bcc0dea566d0dbb797a10
gfumagali <guilhermefumagali@estudante.ufscar.br>
2023-04-12 22:05:09 -0300

    check_keys()

--- tlmanager/config/init_fastapi.py
+++ tlmanager/config/init_fastapi.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from .init_database import mongodb_client, database
+from services.keys import check_keys
 
 app = FastAPI() # to init: uvicorn main:app --reload
 
@@ -7,6 +8,8 @@ app = FastAPI() # to init: uvicorn main:app --reload
 async def startup_event():
     if database is None:
         raise Exception('Database not initialized')
+    if check_keys() is False:
+        raise Exception('Keys not initialized')
     app.mongodb_client = mongodb_client
     app.database = database
     print('Connected to MongoDB')
--- tlmanager/services/keys.py
+++ tlmanager/services/keys.py
@@ -4,6 +4,16 @@ import os
 
 PATH_KEYS = "keys/"
 
+#verify if the keys exist
+def check_keys():
+    if not os.path.exists(PATH_KEYS):
+        return False
+    if not os.path.exists(PATH_KEYS + "private_key.pem"):
+        return False
+    if not os.path.exists(PATH_KEYS + "public_key.pem"):
+        return False
+    return True
+
 def sign_root(root):
     return Ed25519PrivateKey.from_private_bytes(get_private_key_bytes()).sign(root).hex()
 
