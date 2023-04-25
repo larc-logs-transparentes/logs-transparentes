c96d58d2a3039fea52b0e3a6615cddc5d69eae66
gfumagali <guilhermefumarques@gmail.com>
2023-04-25 16:38:25 -0300

    reset database

--- tlmanager/config/init_database.py
+++ tlmanager/config/init_database.py
@@ -1,7 +1,20 @@
 from pymongo import MongoClient
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    RESET: bool = False
+
+settings = Settings()
 
 URL = "mongodb://localhost:27017"
 COLLECTION = "tlmanager"
 
 mongodb_client = MongoClient(URL)
 database = mongodb_client[COLLECTION]
+
+if settings.RESET:
+    database['state'].drop()
+    database['global_tree_roots'].drop()
+    database['global_tree_leaves'].drop()
+    database['consistency_proofs'].drop()
+    print('Database reseted')
