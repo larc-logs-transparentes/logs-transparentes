6ccbc49ae7b751ab90c8c99b4aafd789d3ae6e41
Gabriel <gabriel_schaedler@hotmail.com>
2023-05-24 13:00:17 -0300

    Atualizando pyodide para prova

--- bu_frontend/src/services/pyodide.js
+++ bu_frontend/src/services/pyodide.js
@@ -1,7 +1,8 @@
 import React, { useEffect,useState } from 'react';
 import { Card, CardBody, Col, Row, Button,Table } from 'reactstrap'
 import script from './main.py';
-import verifyinfo from './services.js';
+import getDataProof from './services.js';
+
 
 const runScript = async (code) => {
   const pyodide = await window.loadPyodide({
@@ -13,7 +14,7 @@ const runScript = async (code) => {
     "https://test-files.pythonhosted.org/packages/37/fb/e0cfc7f7cdb8319346dfe8ee93fba1aeaa9e5a2a914b1180a31f5ddb576a/tlverifier-0.0.8-py3-none-any.whl"
   );
   await micropip.install("pymerkle-logsTransparentes");
-  const verificacao=await verifyinfo(1);
+  const verificacao=await getDataProof(1);
   console.log(verificacao.fullproofString)
   const pythonCode = `
   from tlverifier.merkle_functions.tl_functions import verify_inclusion_proof
--- bu_frontend/src/services/services.js
+++ bu_frontend/src/services/services.js
@@ -15,9 +15,9 @@ function getBuById(bu_id) {
       })
 }
 
-function getRoot(){
+export default function getDataProof(id){
     return new Promise(function (resolve, reject){
-        axios.get(`${bu_api_url}/tree/root`)
+        axios.get(`${bu_api_url}/tree/data-proof?tree-name=bu_tree&index=${id}`)
         .then((res) => {
             console.log(res.data)
             resolve(Buffer.from(res.data, 'hex'));
@@ -30,58 +30,4 @@ function getRoot(){
         
     })
    
-}
-function getProofInfo(leafid){
-  return new Promise(function (resolve, reject){
-      axios.get(`${bu_api_url}/tree/leaf/${leafid}`)
-          .then((res) => {
-         // console.log(res.data.proof[0].data)
-          var proofS =  res.data.proof
-          var proofHex = res.data.proofHex
-          for(let i = 0; i < proofS.length; i++){
-              // eslint-disable-next-line
-              if(typeof proofS[i].data.data === 'Array'){
-                  proofS[i] = Buffer.from(JSON.stringify(proofS[i]))
-              }
-              proofS[i].data = Buffer.from(JSON.stringify(proofS[i].data.data))
-          }
-          for(let i = 0; i < proofHex.length; i++){
-              // eslint-disable-next-line
-              if(typeof proofHex[i] === 'string'){
-                  proofS[i].data = Buffer.from(proofHex[i].replace('0x', ''), 'hex')
-              }
-          }
-          var proofLenght= proofS.length
-          var Leaf = res.data.leaf
-          var proofString = res.data.proof
-          var answer = [proofLenght, Leaf, proofS, proofString]
-          resolve(answer);
-          },
-          (err) => {
-              console.log(err)
-              reject(err);
-          }
-      );
-  })
-}
-export default async function verifyinfo(buId){
-    var BU = await getBuById(buId)
-    console.log("bu_verify", BU)
-    var leafid = BU.merkletree_leaf_index
-    var root = await getRoot()
-    var root = root.toString('hex')
-    var fullproof
-    fullproof = await getProofInfo(leafid)
-    var leaf = fullproof[1]
-    var proof = fullproof[2]
-    var proofString = fullproof[3]
-    
-    for(var i = 0; i < proofString.length; i++){
-       //  if(typeof proofString[i] === 'buffer'){
-             proofString[i].data = proofString[i].data.toString('hex')
-         // }
-     }
-    let fullproofString = JSON.stringify(proofString)
-    
-    return { leaf,root,fullproofString}
 }
