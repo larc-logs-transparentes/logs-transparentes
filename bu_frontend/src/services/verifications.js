768252be44d73c0e2c67240ec6216452f258c6d4
Gabriel <gabriel_schaedler@hotmail.com>
2023-06-09 15:30:04 -0300

    Ainda não funciona a verificação

--- bu_frontend/src/services/verifications.js
+++ bu_frontend/src/services/verifications.js
@@ -1,6 +1,11 @@
 import {getRoot,getDataProof,getBuById, getTrustedRoot} from './services.js';
 import initPyodide from './pyodide.js';
 export async function verifySingleData(id){
+    const bubyid=await JSON.stringify((getBuById(id)));
+    const root=await JSON.stringify(getTrustedRoot());
+    const proofData=await JSON.stringify(getDataProof(id));
+    console.log(proofData)
+    console.log(bubyid)
     const pyodide = await initPyodide();
   
     const pythonCode = `
@@ -8,12 +13,13 @@ export async function verifySingleData(id){
     import requests
     import json
     def func():
-      proofData=(${await getDataProof(id)})
+      proofData=json.loads(${proofData})
       #Definindo true do javascript como True do python
       #proofData['local_tree']['inclusion_proof']['metadata']['security']=True
       #proofData['data']['inclusion_proof']['metadata']['security']=True
-      bu=${await getBuById(id)}
-      verifydata =verify_single_data(proofData, ${await getTrustedRoot()},bu['bu_inteiro'])
+      bu=json.loads(${bubyid})
+      root=json.loads(${root})
+      verifydata =verify_single_data(proofData, root['value'],bu['bu_inteiro'])
       return verifydata
     func()
   `;
