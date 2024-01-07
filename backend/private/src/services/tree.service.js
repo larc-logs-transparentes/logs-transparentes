977c0000fa2da36ea368d22f805e03cdbd39a466
gfumagali <guilhermefumarques@gmail.com>
2024-01-07 19:09:13 -0300

    fix: commit all tree variable initialized

--- backend/private/src/services/tree.service.js
+++ backend/private/src/services/tree.service.js
@@ -2,9 +2,10 @@ const tlmanager_adapter = require("../adapters/tlmanager.adapter");
 
 exports.commitAllTree = async () => {
     const trees = await tlmanager_adapter.getTrees()['trees']
+
     for (const tree of trees) {
         console.info(`Commiting tree ${tree}`)
-        await response = tlmanager_adapter.commit(tree)
+        const response = await tlmanager_adapter.commit(tree)
         console.info(response)
     }
 
