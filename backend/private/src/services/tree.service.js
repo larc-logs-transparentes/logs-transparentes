367e4b6d9096bd061a235c01c6cc683be34c6930
gfumagali <guilhermefumarques@gmail.com>
2024-01-07 19:20:01 -0300

    fix: object error

--- backend/private/src/services/tree.service.js
+++ backend/private/src/services/tree.service.js
@@ -1,9 +1,9 @@
 const tlmanager_adapter = require("../adapters/tlmanager.adapter");
 
 exports.commitAllTree = async () => {
-    const trees = await tlmanager_adapter.getTrees()['trees']
+    const trees = await tlmanager_adapter.getTrees()
 
-    for (const tree of trees) {
+    for (const tree of trees['trees']) {
         console.info(`Commiting tree ${tree}`)
         const response = await tlmanager_adapter.commit(tree)
         console.info(response)
