57e775fd184974e477431481276099428ddc52e4
Gabriel <gabriel_schaedler@hotmail.com>
2024-01-23 20:06:19 -0300

    Ajustado o design da page Manual Automático

--- frontend_new/src/pages/SearchBu/components/ManualAutomatic.jsx
+++ frontend_new/src/pages/SearchBu/components/ManualAutomatic.jsx
@@ -7,30 +7,33 @@ import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 
 function ManualAutomatic({ closeModal }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const { id } = useParams(); // Extract id from URL
+  const { id } = useParams(); 
 
   const toggleModal = () => setIsModalOpen(!isModalOpen);
 
   return (
-    <div className='fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center backdrop-blur-md z-50'>
+    <div className='fixed inset-0 bg-black bg-opacity-30 flex justify-center items-center backdrop-blur-md z-50 '>
       {isModalOpen && <SendBu openSendBu={toggleModal} closeModal={closeModal} />}
-
-      <div className='relative bg-white rounded-2xl shadow-xl flex flex-col items-center p-8 w-[550px] h-[350px]'>
+      <div className='relative bg-white rounded-2xl shadow-xl flex flex-col p-8 md2:w-[714px] md2:h-[469px]'>
         <HighlightOffIcon onClick={closeModal} className="absolute top-4 right-4 text-blue-600 cursor-pointer" style={{ width: '32px', height: '32px' }} />
-        <h1 className='text-2xl font-bold mb-6'>Deseja enviar essa verificação para o monitor?</h1>
-        <p className='mb-4 text-center'>Preencha as informações abaixo para enviar para o monitor.</p>
-        <div className='flex align-items-center items-center w-full place-content-center gap-[20%]'>
-          <div className='flex flex-col items-center'>
-            <button onClick={toggleModal} className="flex justify-center bg-white hover:bg-blue-light items-center bg-teal-500 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded-lg ">
-              <ManualIcon className="w-24 h-24" />
-            </button>
-            <p className='mt-2'>Manual</p>
+        <h1 className='text-xl font-bold mb-2'>Deseja enviar essa verificação para o monitor?</h1>
+        <p className='mb-8'>Preencha as informações abaixo para enviar para o monitor.</p>
+        <div className='flex align-items-center items-center place-content-center gap-[2%]'>
+          <div className='flex-col flex'>Manual
+            <div onClick={toggleModal} className='cursor-pointer flex flex-col items-center border-blue-light hover:bg-blue-light border-2 p-2 rounded-2xl text-center text-black md2:h-[242px] md2:w-[320px]'>
+              <button className="">
+                <ManualIcon className="w-24 h-24" />
+              </button>
+              <p className='mt-2 font-bold w-[100%]'>Clique aqui para verificar se os dados batem entre a tela do dispositivo com o papel em mãos</p>
+            </div>
           </div>
-          <div className='flex flex-col items-center'>
-            <button className="flex justify-center items-center bg-white hover:bg-blue-light text-white font-bold py-2 px-4 rounded-lg ">
-              <AutomaticIcon className="w-24 h-24" />
-            </button>
-            <p className='mt-2'>Automatic</p>
+          <div className='flex-col flex'>Automático
+            <div className='cursor-pointer flex flex-col items-center border-blue-light hover:bg-blue-light border-2 p-2 rounded-2xl text-center text-black md2:h-[242px] md2:w-[320px]'>
+              <button className="">
+                <AutomaticIcon className="w-24 h-24" />
+              </button>
+              <p className='mt-2 font-bold'>Clique aqui para abrir a câmera do seu celular e escanear qr-code.</p>
+            </div>
           </div>
         </div>
       </div>
