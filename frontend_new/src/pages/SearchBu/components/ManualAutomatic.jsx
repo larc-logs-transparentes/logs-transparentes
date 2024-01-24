c3edc523a13aea3f26af7b69d4fae4ee33290b24
Gabriel <gabriel_schaedler@hotmail.com>
2024-01-24 13:18:17 -0300

    Acesso a camera funcionando, falta estilizar

--- frontend_new/src/pages/SearchBu/components/ManualAutomatic.jsx
+++ frontend_new/src/pages/SearchBu/components/ManualAutomatic.jsx
@@ -3,24 +3,29 @@ import { ReactComponent as ManualIcon } from '../../../assets/manual-icon.svg';
 import { ReactComponent as AutomaticIcon } from '../../../assets/automatic-icon.svg';
 import { useParams } from 'react-router-dom';
 import SendBu from './SendBu';
+import SendBuAutomatic from './SendBuAutomatic'; // Import SendBuAutomatic
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 
 function ManualAutomatic({ closeModal }) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isManualModalOpen, setIsManualModalOpen] = useState(false);
+  const [isAutomaticModalOpen, setIsAutomaticModalOpen] = useState(false); 
   const { id } = useParams(); 
 
-  const toggleModal = () => setIsModalOpen(!isModalOpen);
+  const toggleManualModal = () => setIsManualModalOpen(!isManualModalOpen);
+  const toggleAutomaticModal = () => setIsAutomaticModalOpen(!isAutomaticModalOpen); 
 
   return (
     <div className='fixed inset-0 bg-black bg-opacity-30 flex justify-center items-center backdrop-blur-md z-50 '>
-      {isModalOpen && <SendBu openSendBu={toggleModal} closeModal={closeModal} />}
+      {isManualModalOpen && <SendBu openSendBu={toggleManualModal} closeModal={closeModal} />}
+      {isAutomaticModalOpen && <SendBuAutomatic closeAutomaticModal={toggleAutomaticModal} />} 
+      
       <div className='relative bg-white rounded-2xl shadow-xl flex flex-col p-8 md2:w-[714px] md2:h-[469px]'>
         <HighlightOffIcon onClick={closeModal} className="absolute top-4 right-4 text-blue-600 cursor-pointer" style={{ width: '32px', height: '32px' }} />
         <h1 className='text-xl font-bold mb-2'>Deseja enviar essa verificação para o monitor?</h1>
         <p className='mb-8'>Preencha as informações abaixo para enviar para o monitor.</p>
         <div className='flex align-items-center items-center place-content-center gap-[2%]'>
           <div className='flex-col flex'>Manual
-            <div onClick={toggleModal} className='cursor-pointer flex flex-col items-center border-blue-light hover:bg-blue-light border-2 p-2 rounded-2xl text-center text-black md2:h-[242px] md2:w-[320px]'>
+            <div onClick={toggleManualModal} className='cursor-pointer flex flex-col items-center border-blue-light hover:bg-blue-light border-2 p-2 rounded-2xl text-center text-black md2:h-[242px] md2:w-[320px]'>
               <button className="">
                 <ManualIcon className="w-24 h-24" />
               </button>
@@ -28,7 +33,7 @@ function ManualAutomatic({ closeModal }) {
             </div>
           </div>
           <div className='flex-col flex'>Automático
-            <div className='cursor-pointer flex flex-col items-center border-blue-light hover:bg-blue-light border-2 p-2 rounded-2xl text-center text-black md2:h-[242px] md2:w-[320px]'>
+            <div onClick={toggleAutomaticModal} className='cursor-pointer flex flex-col items-center border-blue-light hover:bg-blue-light border-2 p-2 rounded-2xl text-center text-black md2:h-[242px] md2:w-[320px]'>
               <button className="">
                 <AutomaticIcon className="w-24 h-24" />
               </button>
