b6e60337cd191b06c7a365524c6c16813ff9c9bb
gfumagali <guilhermefumarques@gmail.com>
2024-03-14 21:13:35 -0300

    feat: adding reset command line parameter

--- backend/bu_service/app/database/connection.py
+++ backend/bu_service/app/database/connection.py
@@ -1,7 +1,23 @@
-from pymongo import MongoClient
+import os
 
-connection = MongoClient('mongodb://localhost:27017/')  # externalizar
+import dotenv
+from pymongo import MongoClient
+from pydantic_settings import BaseSettings
+
+dotenv.load_dotenv()
+
+
+class Settings(BaseSettings):
+    RESET: bool = False
+
+
+settings = Settings()
+
+connection = MongoClient(os.getenv('MONGO_URL'))
+if settings.RESET:
+    connection.drop_database('bu_service')
+    print('Database reseted')
 
 
 def get_db():
-    return connection['bu_service']  # externalizar
+    return connection['bu_service']
