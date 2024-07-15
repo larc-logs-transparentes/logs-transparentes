8bcae6d72fee8b56eed982b39fe7fad7017ac471
claudiolcsilva <claudiolcsilva@outlook.com>
2024-07-15 10:46:38 -0300

    cards gerados dinamicamente

--- frontend_new/src/pages/Dashboard/components/Cards.jsx
+++ frontend_new/src/pages/Dashboard/components/Cards.jsx
@@ -1,22 +1,41 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import dashboardglobaltree from '../../../assets/dashboardglobaltree.svg';
 import dashboardBUtree from '../../../assets/dashboardBU.svg';
 import Modal from './Modal';
 
 const Cards = () => {
-    const cardInfo = [
-        { title: "Árvore Global", subtitle: "global", description: "Contém a raiz de todas as árvores locais, garante a integridade de todos os dados eletorais.", downloadUrl: "http://localhost:8080/tree/all-leaf-data-global-tree"},
-        { title: "Árvore 545", subtitle: "545" },
-        { title: "Árvore aaa", subtitle: "aaa"},
-        { title: "Árvore bbb", subtitle: "bbb" },
-        { title: "Árvore ccc", subtitle: "ccc"},
-        { title: "Árvore ddd", subtitle: "ddd"}
-    ];
-
+    const [cardInfo, setCardInfo] = useState([]);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [selectedCard, setSelectedCard] = useState(null);
-    
 
+    useEffect(() => {
+        const fetchTreeData = async () => {
+            try {
+                const response = await fetch('http://localhost:8080/tree');
+                if (!response.ok) {
+                    throw new Error('Failed to fetch tree data');
+                }
+                const data = await response.json();
+                const formattedTrees = formatTreeData(data.trees);
+                setCardInfo(formattedTrees);
+            } catch (error) {
+                console.error('Error fetching tree data:', error);
+            }
+        };
+
+        fetchTreeData();
+    }, []);
+
+    const formatTreeData = (trees) => {
+        return trees.map((treeName) => ({
+            title: treeName,
+            subtitle: treeName.slice(-3),
+            description: `Raiz da árvore ${treeName}`,
+            downloadUrl: `http://localhost:8080/tree/all-roots-global-tree`,
+            hash: `http://localhost:8080/tree/tree-root?tree_name=${treeName}`,
+            allroots: `http://localhost:8080/tree/all-roots-global-tree`
+        }));
+    };
 
     const handleCardClick = (card) => {
         setSelectedCard(card);
@@ -29,16 +48,16 @@ const Cards = () => {
                 <div key={index} className="w-full flex justify-center p-4" onClick={() => handleCardClick(card)}>
                     <div className="w-[70vw] md:w-[1184px] h-[135px] mx-auto bg-[#1094AB] rounded-lg p-6 shadow-md cursor-pointer">
                         <img src={dashboardglobaltree} alt="Ícone da Árvore Global" className="w-18.5 h-16 rounded mx-auto" />
-                        <h2 className="text-center text-white font-inter font-size-24px font-semibold text-2xl ">{card.title}</h2>
+                        <h2 className="text-center text-white font-inter font-size-24px font-semibold text-2xl "> Árvore global</h2>
                     </div>
                 </div>
             );
         } else {
             return (
-                <div key={index} className="w-full md:w-1/3 flex justify-center p-2" onClick={() => handleCardClick(card)}>
+                <div key={index} className="w-full md:w-[362.67px] flex justify-center p-2" onClick={() => handleCardClick(card)}>
                     <div className="md:h-[127px] w-[362.67px] mx-auto bg-[#00C6D4] rounded-lg p-4 shadow-sm cursor-pointer">
                         <img src={dashboardBUtree} alt="Ícone das Árvore de BUS" className="w-16 h-14.5 rounded mx-auto" />
-                        <h2 className="text-2xl text-black font-inter font-size-20px font-semibold text-center mb-2">{card.title}</h2>
+                        <h2 className="text-2xl text-black font-inter font-size-20px font-semibold text-center mb-2">Árvore {card.subtitle}</h2>
                     </div>
                 </div>
             );
@@ -50,7 +69,7 @@ const Cards = () => {
             <div className="w-[80vw] md:w-[1232px] mx-auto flex flex-col justify-center border-2 border-blue-light rounded-2xl p-5 space-y-4">
                 <h1 className="text-2xl text-blue-light font-inter font-size-24px text-left mb-4">Dashboard</h1>
                 <div className="flex flex-col items-center space-y-4">
-                    {renderCard(cardInfo[0], 0)}
+                    {cardInfo.length > 0 && renderCard(cardInfo[0], 0)}
                     <div className="flex flex-wrap justify-center">
                         {cardInfo.slice(1).map((card, index) => renderCard(card, index + 1))}
                     </div>
--- frontend_new/src/pages/Dashboard/components/Modal.jsx
+++ frontend_new/src/pages/Dashboard/components/Modal.jsx
@@ -1,17 +1,77 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import modalclosebutton from '../../../assets/modalclosebutton.svg';
 import modalbaixarfolhasbutton from '../../../assets/modalbaixarfolhasbutton.svg';
 
-const Modal = ({ isOpen, onClose, card}) => {
-  if (!isOpen) return null;
+const Modal = ({ isOpen, onClose, card }) => {
+  const [hashValue, setHashValue] = useState('');
+  const [numeroValue, setNumeroValue] = useState('');
+  const [timestampValue, setTimestampValue] = useState('');
+
+  useEffect(() => {
+    const fetchHashValue = async () => {
+      if (card && card.hash) {
+        try {
+          const response = await fetch(card.hash);
+          const data = await response.json();
+          setHashValue(data.value);
+        } catch (error) {
+          console.error('Erro ao buscar valor do hash:', error);
+          setHashValue('Erro ao carregar valor');
+        }
+      }
+    };
+
+    const fetchNumeroValue = async () => {
+      if (card && card.allroots) {
+        try {
+          const response = await fetch(card.allroots);
+          const data = await response.json();
+          const lastTreeSize = data.roots[data.roots.length - 1].tree_size;
+          setNumeroValue(lastTreeSize);
+        } catch (error) {
+          console.error('Erro ao buscar valor do número:', error);
+          setNumeroValue('Erro ao carregar valor');
+        }
+      }
+    };
+
+    const fetchTimestampValue = async () => {
+      if (card && card.allroots) {
+        try {
+          const response = await fetch(card.allroots);
+          const data = await response.json();
+          const lastTimestamp = data.roots[data.roots.length - 1].timestamp;
+          const formattedDate = new Date(lastTimestamp).toLocaleString('pt-BR', {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit'
+          });
+          setTimestampValue(formattedDate);
+        } catch (error) {
+          console.error('Erro ao buscar valor do timestamp:', error);
+          setTimestampValue('Erro ao carregar valor');
+        }
+      }
+    };
+
+    fetchHashValue();
+    fetchNumeroValue();
+    fetchTimestampValue();
+  }, [card]);
+
+  if (!isOpen || !card) return null;
 
   const downloadLeaf = () => {
     if (card.downloadUrl) {
       const link = document.createElement('a');
       link.href = card.downloadUrl;
-      link.download = `Folhas da ${card.title}.json`; 
+      link.download = `Folhas da ${card.title}.json`;
       document.body.appendChild(link);
+      link.target = "_blank";
       link.click();
       document.body.removeChild(link);
     } else {
@@ -23,38 +83,32 @@ const Modal = ({ isOpen, onClose, card}) => {
     <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-50">
       <div className="md:w-[900px] h-[381px] bg-white p-6 rounded-lg border border-[#1094AB] shadow-lg w-96">
         <div className="relative md:w-[852px] h-[29px] mb-4">
-        <h2 className="text-2xl font-inter font-size-24px text-[#1094AB] mb-4">{card.title}
-            <button 
-            className="absolute top-0 right-0"
-            onClick={onClose}
-            >
-            <img src={modalclosebutton} alt="Botão de fechar" className="w-6 h-6 rounded" />
+          <h2 className="text-2xl font-inter font-size-24px text-[#1094AB] mb-4">
+            Árvore {card.title}
+            <button className="absolute top-0 right-0" onClick={onClose}>
+              <img src={modalclosebutton} alt="Botão de fechar" className="w-6 h-6 rounded" />
             </button>
-        </h2>
+          </h2>
         </div>
         <p className="font-inter font-size-16px text-black mb-4">{card.description}</p>
-        
         <div className="md:w-[852px] h-[154px] rounded-lg flex flex-col gap-4">
-            <div className="md:w-[852px] h-[81px] rounded-lg border border-gray">
+          <div className="md:w-[852px] h-[81px] rounded-lg border border-gray">
             <h1 className="text-[#1094AB] font-inter font-size-14px text-left ml-2">Raiz {card.subtitle}</h1>
-            </div>
-    
-            <div className="md:w-[852px] h-[57px] rounded-lg border border-gray">
+            <h2 className="text-[#979797] font-inter font-size-15px ml-12">Hash: {hashValue}</h2>
+            <h2 className="text-[#979797] font-inter font-size-15px ml-1.5">Gerado em: {timestampValue}</h2>
+          </div>
+          <div className="md:w-[852px] h-[57px] rounded-lg border border-gray">
             <h1 className="text-[#1094AB] font-inter font-size-14px text-left ml-2">Elementos</h1>
-            </div>
+            <h2 className="text-[#979797] font-inter font-size-15px ml-6">Número: {numeroValue}</h2>
+          </div>
         </div>
-
         <div className="relative md:w-[852px] h-[29px] mb-4">
-        <h2 className="text-2xl font-inter font-size-24px text-[#1094AB] mb-4">
-            <button 
-            className="absolute bottom-0 left-0 gap-4"
-            onClick={downloadLeaf}
-            >
-            <img src={modalbaixarfolhasbutton} alt="Botão de baixar folhas" className="w-[131px] h-[35px] " />
+          <h2 className="text-2xl font-inter font-size-24px text-[#1094AB] mb-4">
+            <button className="absolute bottom-0 left-0 gap-4" onClick={downloadLeaf}>
+              <img src={modalbaixarfolhasbutton} alt="Botão de baixar folhas" className="w-[131px] h-[35px]" />
             </button>
-        </h2>
+          </h2>
         </div>
-
       </div>
     </div>,
     document.body
