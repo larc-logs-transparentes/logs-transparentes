e232448be42c9f2f43819e0d33425a6cc3fdf82e
Luiz Felipe Mol Santos <felipe.mol@usp.br>
2024-07-31 17:09:36 -0300

    chave-publica

--- backend/public/src/controllers/public_key.controller.js
+++ backend/public/src/controllers/public_key.controller.js
@@ -1,5 +1,5 @@
 const router = require("express").Router()
-const tlmanager_adapter = require("../src/adapters/tlmanager.adapter")
+const tlmanager_adapter = require("../adapters/tlmanager.adapter")
 
 router.get("", async (req, res) => {
     console.info(`[public_key] GET public_key`)
