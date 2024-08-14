c9e15d46922803a720bbfc1a4936c4f08f8e4c26
Leonardo Kimura <lkimura@larc2.usp>
2024-08-14 12:56:10 -0300

    format SearchBU and BUResult

--- frontend_new/src/App.js
+++ frontend_new/src/App.js
@@ -1,19 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import HomePage from './pages/Homepage/HomePage';
-import SearchBu from './pages/SearchBu/SearchBu.jsx';
-import './index.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/Homepage/HomePage";
+import SearchBu from "./pages/SearchBu/SearchBu.jsx";
+import "./index.css";
 
 function App() {
   return (
     <Router>
-      <div className='overflow-x-hidden custom-scrollbar'>
+      <div className="overflow-x-hidden custom-scrollbar">
         <Navbar />
         <Routes>
-          <Route path="/" element={<HomePage />} /> 
-          <Route path="/:electionId" element={<HomePage />} /> 
+          <Route path="/" element={<HomePage />} />
+          <Route path="/:electionId" element={<HomePage />} />
           <Route path="/:electionId/search/:id" element={<SearchBu />} />
-          <Route path="/:electionId/search" element={<SearchBu />} /> 
+          <Route path="/:electionId/search" element={<SearchBu />} />
         </Routes>
       </div>
     </Router>
