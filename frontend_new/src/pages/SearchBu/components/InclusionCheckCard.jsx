29338ccc46b7a6552cd97e063604659dad27e762
mariaeduardanichelle <madunichelle@gmail.com>
2024-09-04 10:14:09 -0300

    feat: modal close on outside click

--- frontend_new/src/pages/SearchBu/components/InclusionCheckCard.jsx
+++ frontend_new/src/pages/SearchBu/components/InclusionCheckCard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import ErrorIcon from '../../../assets/ErrorIcon.svg';
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
@@ -7,6 +7,7 @@ const InclusionCheckCard = ({ closeModal, bu, proof, isProofTrue }) => {
   const [lastRoot, setLastRoot] = useState({ value: '', timestamp: '' }); 
   const [buHash, setBuHash] = useState('');
   const navigate = useNavigate(); 
+  const modalRef = useRef();
 
   useEffect(() => {
     const run = async () => {
@@ -17,6 +18,19 @@ const InclusionCheckCard = ({ closeModal, bu, proof, isProofTrue }) => {
 
     run();
   }, []);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        closeModal();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [closeModal]);
 
   function downloadDataProof() {
     const json = JSON.stringify(proof, null, 2);
@@ -35,7 +49,7 @@ const InclusionCheckCard = ({ closeModal, bu, proof, isProofTrue }) => {
 
   return (
     <div className='fixed inset-0 bg-black bg-opacity-50 flex backdrop-blur z-30 justify-center items-center'>
-      <div className='relative sm:w-full md:w-[700px] lg:w-[800px] min-h-full md:min-h-[320px] border-2 border-blue rounded-2xl bg-white p-8'> 
+      <div className='relative sm:w-full md:w-[700px] lg:w-[800px] min-h-full md:min-h-[320px] border-2 border-blue rounded-2xl bg-white p-8' ref={modalRef}> 
         <div className="absolute top-3 right-3">
           <HighlightOffIcon onClick={closeModal} className="text-blue cursor-pointer mr-[16px]" style={{ width: '32px', height: '32px' }}/>
         </div>
@@ -62,7 +76,6 @@ const InclusionCheckCard = ({ closeModal, bu, proof, isProofTrue }) => {
             )
           }
           
-        
           <h2 className='md:text-center md:mt-2 text-md mb-2 md:mb-0 relative font-sans font-bold text-yellow cursor-pointer' onClick={navigateToInclusion}>Saiba Mais</h2>
         </div>
 
@@ -88,8 +101,6 @@ const InclusionCheckCard = ({ closeModal, bu, proof, isProofTrue }) => {
             <div>Tamanho: {proof.local_tree.local_root.tree_size}</div> 
           </div>
 
-
-          
         </div>
 
         <div className="flex gap-4">
