7efcd76f3aa18864dbba6a13e64f79740f86a9a5
claudiolcsilva <claudiolcsilva@outlook.com>
2024-10-23 19:53:30 -0300

    populate_db corrigido

--- bu_utils/gov_codes/assinatura_hash.py
+++ bu_utils/gov_codes/assinatura_hash.py
@@ -1,13 +1,24 @@
+"""Verifica os hashes dos arquivos de resultado da Urna"""
+
 import argparse
 import hashlib
 import logging
 import os
 import sys
+from typing import Tuple
 
-import asn1tools
+from asn1tools import compile_files
+from asn1tools.compiler import Specification
+from ecpy.ecdsa import ECDSA
+from ecpy.eddsa import EDDSA
+from ecpy.keys import ECPublicKey
+from lib.mr_util import (extrai_chave_certificado_hw, extrai_entidade_decoded,
+                         valida_certificado)
 
 
 def verifica_hash(origem: str, resumo: bytes, conteudo: bytes) -> str:
+    """Verifica se o hash do conteúdo é igual ao hash esperado"""
+
     sha = hashlib.sha512()
     sha.update(conteudo)
     digest = sha.digest()
@@ -19,6 +30,8 @@ def verifica_hash(origem: str, resumo: bytes, conteudo: bytes) -> str:
 
 
 def verifica_hash_arquivo(path: str, resumo: bytes) -> str:
+    """Lê o conteúdo do arquivo e verifica se o hash é o esperado"""
+
     with open(path, "rb") as file:
         conteudo = file.read()
 
@@ -26,18 +39,20 @@ def verifica_hash_arquivo(path: str, resumo: bytes) -> str:
 
 
 def verifica_hashes_arquivos(dir_arqs: str, assinaturas: dict):
+    """Verifica os hashes dos arquivos de listados em `assinaturas`"""
+
     erros = 0
-    for assinaturaArquivo in assinaturas["arquivosAssinados"]:
-        nome_arquivo = assinaturaArquivo["nomeArquivo"]
+    for assinatura_arquivo in assinaturas["arquivosAssinados"]:
+        nome_arquivo = assinatura_arquivo["nomeArquivo"]
         path = os.path.join(dir_arqs, nome_arquivo)
-        logging.info("Validando arquivo %s", nome_arquivo)
+        logging.info("Validando hash do arquivo %s", nome_arquivo)
 
         if not os.path.isfile(path):
             logging.error("Arquivo não encontrado %s", nome_arquivo)
             erros += 1
             continue
 
-        resumo = assinaturaArquivo["assinatura"]["hash"]
+        resumo = assinatura_arquivo["assinatura"]["hash"]
         res = verifica_hash_arquivo(path, resumo)
         if not res[0]:
             logging.error("Hash do arquivo %s está incorreto", nome_arquivo)
@@ -47,65 +62,126 @@ def verifica_hashes_arquivos(dir_arqs: str, assinaturas: dict):
     return erros
 
 
-def verifica_hashes_de(path: str, conv, envelope: dict, campo: str):
-    entidade_assinatura = envelope[campo]
+def verifica_hashes_de(path: str, ent_assinatura: dict, campo: str, conv: Specification):
+    """Verifica os hashes dos listados no campo `campo` do envelope"""
+
+    entidade_assinatura = ent_assinatura[campo]
     assinaturas_encoded = entidade_assinatura["conteudoAutoAssinado"]
 
     erros = 0
     auto_hash = entidade_assinatura["autoAssinado"]["assinatura"]["hash"]
     res = verifica_hash("do conteúdo", auto_hash, assinaturas_encoded)
     if not res[0]:
-        logging.error("Validando %s - hash do conteúdo do aruqivo de assinatura está incorreto", campo)
+        logging.error(
+            "Validando %s - hash do conteúdo do aruqivo de assinatura está incorreto", campo)
         print(f"{res[1]}")
         erros += 1
-    else:
-        logging.info("Validando %s - hash do conteúdo do aruqivo de assinatura está correto", campo)
 
     assinaturas_decoded = conv.decode("Assinatura", assinaturas_encoded)
     erros += verifica_hashes_arquivos(path, assinaturas_decoded)
     return erros
 
 
-def verifica_hashes(asn1_paths: list, assinatura_path: str):
-    conv = asn1tools.compile_files(asn1_paths, codec="ber", numeric_enums=True)
-    with open(assinatura_path, "rb") as file:
-        envelope_encoded = bytearray(file.read())
-    envelope_decoded = conv.decode("EntidadeAssinaturaResultado", envelope_encoded)
-    dir_arquivos = os.path.dirname(assinatura_path)
+def verifica_hashes(dir_arquivos: str, ent_assinatura: dict, conv: Specification) -> int:
+    """Verifica os hashes dos arquivos de listados no arquivo `assinatura_path`"""
     erros = 0
-    erros += verifica_hashes_de(dir_arquivos, conv, envelope_decoded, "assinaturaSW")
-    erros += verifica_hashes_de(dir_arquivos, conv, envelope_decoded, "assinaturaHW")
+    erros += verifica_hashes_de(dir_arquivos,
+                                ent_assinatura, "assinaturaSW", conv)
+    erros += verifica_hashes_de(dir_arquivos,
+                                ent_assinatura, "assinaturaHW", conv)
     return erros
 
 
+def valida_assinturas(dir_arquivos: str, conv: Specification, ent_assinatura: dict,
+                      x509: dict, chave: ECPublicKey, verificador: (ECDSA | EDDSA)) -> int:
+    """Valida as assinaturas dos arquivos"""
+    erros = 0
+
+    logging.info("Validando assinatura do certificado")
+    if not valida_certificado(x509, verificador):
+        logging.error("Certificado inválido")
+        erros += 1
+
+    conteudo_auto_ass = conv.decode(
+        'Assinatura', ent_assinatura["assinaturaHW"]["conteudoAutoAssinado"])
+    arquivos_assinados = conteudo_auto_ass["arquivosAssinados"]
+    for arquivo in arquivos_assinados:
+        nome_arquivo = arquivo["nomeArquivo"]
+        hash_arquivo = arquivo["assinatura"]["hash"]
+        ass_arquivo = arquivo["assinatura"]["assinatura"]
+        logging.info("Validando assinatura do arquivo: %s", nome_arquivo)
+        logging.debug("Hash do arquivo: %s", hash_arquivo.hex().upper())
+        logging.debug("Assinatura do arquivo: %s", ass_arquivo.hex().upper())
+
+        path_arquivo = os.path.join(dir_arquivos, nome_arquivo)
+        if not os.path.exists(path_arquivo):
+            logging.error("Arquivo não encontrado %s", path_arquivo)
+            erros += 1
+            continue
+
+        if not verificador.verify(hashlib.sha512(hash_arquivo).digest(), ass_arquivo, chave):
+            logging.error("Assinatura inválida %s", path_arquivo)
+            erros += 1
+            continue
+
+    return erros
+
+
+def le_arq_assinaturas(asn1_path: str, assinatura_path: str) -> Tuple[Specification, dict]:
+    """Carrega o arquivo de assinaturas da urna para as validações"""
+
+    conv = compile_files([asn1_path], codec="ber", numeric_enums=True)
+    with open(assinatura_path, "rb") as file:
+        envelope_encoded = bytearray(file.read())
+    envelope_decoded = conv.decode(
+        "EntidadeAssinaturaEcourna", envelope_encoded)
+    return conv, envelope_decoded
+
+
 def main():
-    parser = argparse.ArgumentParser(
-        description="Le o arquivo de assinaturas da Urna Eletrônica (UE) e verifica os hashes dos arquivos de resultado",
-        formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument("-a", "--asn1", nargs="+", required=True,
+    """Ponto de entrada do programa"""
+
+    parser = argparse.ArgumentParser(description="Valida o arquivo de assinaturas Urna",
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument("-a", "--asn1", type=str, required=True,
                         help="Caminho para o arquivo de especificação asn1 das assinaturas")
     parser.add_argument("-s", "--assinatura", type=str, required=True,
                         help="Caminho para o arquivo de assinatura originado na UE")
-    parser.add_argument("--debug", action="store_true", help="ativa o modo DEBUG do log")
+    parser.add_argument("--debug", action="store_true",
+                        help="ativa o modo DEBUG do log")
 
     args = parser.parse_args()
 
     assinatura_path = args.assinatura
-    asn1_paths = args.asn1
+    asn1_path = args.asn1
     level = logging.DEBUG if args.debug else logging.INFO
-    logging.basicConfig(level=level, format="%(asctime)s - %(levelname)s - %(message)s")
+    logging.basicConfig(
+        level=level, format="%(asctime)s - %(levelname)s - %(message)s")
 
-    logging.info("Processa %s com as especificações %s", assinatura_path, asn1_paths)
+    logging.info("Valida as assinaturas dos arquivos de resultado")
+    logging.info(" * arquivo de assinaturas %s", assinatura_path)
+    logging.info(" * especificação do arquivo de assinaturas %s", asn1_path)
     if not os.path.exists(assinatura_path):
-        logging.error("Arquivo de assinaturas (%s) não encontrado", assinatura_path)
+        logging.error(
+            "Arquivo de assinaturas (%s) não encontrado", assinatura_path)
         sys.exit(-1)
-    for asn1_path in asn1_paths:
-        if not os.path.exists(asn1_path):
-            logging.error(
-                "Arquivo de especificação das assinaturas (%s) não encontrado", asn1_path)
-            sys.exit(-1)
+    if not os.path.exists(asn1_path):
+        logging.error(
+            "Arquivo de especificação das assinaturas (%s) não encontrado", asn1_path)
+        sys.exit(-1)
 
-    erros = verifica_hashes(asn1_paths, assinatura_path)
+    dir_arquivos = os.path.dirname(assinatura_path)
+
+    ent_assinatura, conv = extrai_entidade_decoded(
+        asn1_path, assinatura_path, "EntidadeAssinaturaEcourna")
+
+    erros = verifica_hashes(dir_arquivos, ent_assinatura, conv)
+
+    x509, _, chave, verificador = extrai_chave_certificado_hw(
+        asn1_path, assinatura_path)
+
+    erros += valida_assinturas(dir_arquivos, conv,
+                               ent_assinatura, x509, chave, verificador)
 
     if erros == 0:
         logging.info("Validação terminada com sucesso")
--- bu_utils/gov_codes/rdv_dump.py
+++ bu_utils/gov_codes/rdv_dump.py
@@ -1,86 +1,56 @@
+"""Imprime o conteúdo do arquivo do RDV"""
+
 import argparse
 import logging
 import os
 import sys
 
-import asn1tools
+from lib.mr_util import extrai_entidade_decoded, print_membro
 
 
-def espacos(profundidade: int):
-    return ".   " * profundidade
+def processa_rdv(asn1_path: str, rdv_path: str):
+    """Processa o RDV"""
 
-
-def valor_membro(membro):
-    if isinstance(membro, (bytes, bytearray)):
-        return bytes(membro).hex()
-    return membro
-
-
-def print_list(lista: list, profundidade: int):
-    indent = espacos(profundidade)
-    for membro in lista:
-        if type(membro) is dict:
-            print_dict(membro, profundidade + 1)
-        else:
-            print(f"{indent}valor_membro(membro)")
-
-
-def print_dict(entidade: dict, profundidade: int):
-    for key in sorted(entidade):
-        membro = entidade[key]
-        print_membro(membro, key, profundidade)
-
-
-def print_membro(membro, key, profundidade: int):
-    indent = espacos(profundidade)
-    if type(membro) is dict:
-        print(f"{indent}{key}:")
-        print_dict(membro, profundidade + 1)
-    elif type(membro) is list:
-        print(f"{indent}{key}: [")
-        print_list(membro, profundidade + 1)
-        print(f"{indent}] <== {key}")
-    elif type(membro) is tuple:
-        print_membro(membro[1], f"{key} ({membro[0]})", profundidade)
-    else:
-        print(f"{indent}{key} = {valor_membro(membro)}")
-
-
-def processa_rdv(asn1_paths: list, rdv_path: str):
-    conv = asn1tools.compile_files(asn1_paths, codec="ber")
-    with open(rdv_path, "rb") as file:
-        rdv_encoded = bytearray(file.read())
-    rdv_decoded = conv.decode("EntidadeResultadoRDV", rdv_encoded)
-    print_membro(rdv_decoded, "EntidadeResultadoRDV", 0)
+    rdv, _ = extrai_entidade_decoded(
+        asn1_path, rdv_path, "EntidadeResultadoRDV")
+    print_membro(rdv, "EntidadeResultadoRDV", 0)
 
 
 def main():
+    """Ponto de entrada do programa"""
+
     parser = argparse.ArgumentParser(
-        description="Le o arquivo de Registro Digital do Voto (RDV) da Urna Eletrônica (UE) e imprime um extrato",
+        description="Lê o Registro Digital do Voto (RDV) da Urna e imprime seu conteúdo",
         formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument("-a", "--asn1", nargs="+", required=True,
+    parser.add_argument("-a", "--asn1", type=str, required=True,
                         help="Caminho para o arquivo de especificação asn1 do RDV")
     parser.add_argument("-r", "--rdv", type=str, required=True,
                         help="Caminho para o arquivo de RDV originado na UE")
-    parser.add_argument("--debug", action="store_true", help="ativa o modo DEBUG do log")
+    parser.add_argument("--debug", action="store_true",
+                        help="Ativa o modo DEBUG do log")
 
     args = parser.parse_args()
 
     rdv_path = args.rdv
-    asn1_paths = args.asn1
+    asn1_path = args.asn1
     level = logging.DEBUG if args.debug else logging.INFO
-    logging.basicConfig(level=level, format="%(asctime)s - %(levelname)s - %(message)s")
+    logging.basicConfig(
+        level=level, format="%(asctime)s - %(levelname)s - %(message)s")
 
-    logging.info("Converte %s com as especificações %s", rdv_path, asn1_paths)
+    logging.info("Imprime o conteúdo do RDV")
+    logging.info(" * arquivo do RDV %s", rdv_path)
+    logging.info(" * especificação do arquivo do RDV %s", asn1_path)
     if not os.path.exists(rdv_path):
         logging.error("Arquivo do RDV (%s) não encontrado", rdv_path)
         sys.exit(-1)
-    for asn1_path in asn1_paths:
-        if not os.path.exists(asn1_path):
-            logging.error("Arquivo de especificação do RDV (%s) não encontrado", asn1_path)
-            sys.exit(-1)
+    if not os.path.exists(asn1_path):
+        logging.error(
+            "Arquivo de especificação do RDV (%s) não encontrado", asn1_path)
+        sys.exit(-1)
 
-    processa_rdv(asn1_paths, rdv_path)
+    processa_rdv(asn1_path, rdv_path)
+
+    logging.info("Processamento terminado com sucesso")
 
 
 if __name__ == "__main__":
